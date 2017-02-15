58749d434e4fb977ce4987156fee1a7d62897f27
mikessh <mikhail.shugay@gmail.com>
2017-02-16 02:02:11 +0300

    Some downstream testing

---
+++ test/run.sh
@@ -0,0 +1,8 @@
+MIGEC="java -Xmx2G -jar ../target/migec-*.jar"
+
+$MIGEC Checkout -ou barcodes.txt trb_R1.fastq.gz trb_R2.fastq.gz checkout/
+$MIGEC Histogram checkout/ histogram/
+$MIGEC AssembleBatch --force-overseq 5 --force-collision-filter --default-mask 0:1 checkout/ histogram/ assemble/
+$MIGEC CdrBlastBatch -R TRB checkout/ assemble/ cdrblast/
+$MIGEC FilterCdrBlastResultsBatch cdrblast/ cdrfinal/
+$MIGEC Report .
